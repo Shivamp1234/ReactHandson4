15b27d92794a8322d1b91d82dea793365c29521b
Shivam Patil <65221535+Shivamp1234@users.noreply.github.com>
2023-04-03 23:45:13 +0530

    Update Student.js

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -24,49 +24,49 @@ const Student = () => {
                         <td>26</td>
                         <td>Mern</td>
                         <td>October</td>
-                        <td><a href="#">Edit</a></td>
+                        <td>Edit</td>
                     </tr>
                     <tr>
                         <td>Doe</td>
                         <td>25</td>
                         <td>Mern</td>
                         <td>November</td>
-                        <td><a href="#">Edit</a></td>
+                        <td>Edit</td>
                     </tr>
                     <tr>
                         <td>Biden</td>
                         <td>26</td>
                         <td>Mern</td>
                         <td>September</td>
-                        <td><a href="#">Edit</a></td>
+                        <td>Edit</td>
                     </tr>
                     <tr>
                         <td>Barar</td>
                         <td>22</td>
                         <td>Mern</td>
                         <td>September</td>
-                        <td><a href="#">Edit</a></td>
+                        <td>Edit</td>
                     </tr>
                     <tr>
                         <td>Chris</td>
                         <td>23</td>
                         <td>Mern</td>
                         <td>October</td>
-                        <td><a href="#">Edit</a></td>
+                        <td>Edit</td>
                     </tr>
                     <tr>
                         <td>Flent</td>
                         <td>23</td>
                         <td>Mern</td>
                         <td>November</td>
-                        <td><a href="#">Edit</a></td>
+                        <td>Edit</td>
                     </tr>
                     <tr>
                         <td>Harshita Sharma</td>
                         <td>24</td>
                         <td>Mern</td>
                         <td>October</td>
-                        <td><a href="#">Edit</a></td>
+                        <td>Edit</td>
                     </tr>
                 </tbody>
             </table>
